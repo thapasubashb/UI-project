b95150ac65212e9186cf330b8a1b89fbbcd579c1
Subash B <thapasubash9072@gmail.com>
2026-07-26 20:45:59 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Section1 from "./components/section1/Section1";
 import Section2 from "./components/section2/Section2";
 
+
 const App = () => {
   const users = [
     {
@@ -28,6 +29,8 @@ const App = () => {
     <>
       <Section1 users={users} />
       <Section2 />
+  
+
     </>
   );
 };
---
+++ src/section3/Section3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section3 = () => {
+  return (
+    <div className="h-Screen w-full bg-black ">Section3</div>
+  )
+}
+
+export default Section3
